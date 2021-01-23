16469dfad86846138f594ceec780db27039c06cd
Alejandro Amaral <aamaral@logico.com.ar>
2021-01-23 17:15:02 -0300

    Executable
    Get git status
    
    Fixed bug
    
    First test
    
    Working
    
    Fixed bug
    
    Minor bug
    
    Test
    
    Working

--- src/git-status.sh
+++ src/git-status.sh
@@ -1,2 +1,9 @@
-
-git diff-index --quiet --cached HEAD --
+#!/bin/env bash
+cd $1
+BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+STATUS=$(git status --porcelain 2>/dev/null| egrep "^(M| M)" | wc -l)
+if test "$STATUS" = "0"; then
+  echo "#[fg=green,bg=brightblack,nobold,noitalics,nounderscore]#[fg=black,bg=green,bold]  $BRANCH "
+else
+  echo "#[fg=red,bg=brightblack,nobold,noitalics,nounderscore]#[fg=black,bg=red,bold]  $BRANCH "
+fi
