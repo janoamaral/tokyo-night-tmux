c6d22d71907a5e6e87e2132fd77b5ecca1d8491d
Alejandro Amaral <aamaral@logico.com.ar>
2023-12-12 20:02:34 -0300

    clean up

--- src/git-status.sh
+++ src/git-status.sh
@@ -9,9 +9,9 @@ STATUS_DELETIONS="#[fg=red,bg=black,bold] "
 
 
 if test "$STATUS" != "0"; then
-  CHANGED_COUNT="$(git diff --shortstat | tr "," "\n" | grep "changed" | cut -d" " -f2)"
-  INSERTIONS_COUNT="$(git diff --shortstat | tr "," "\n" | grep "insertion" | cut -d" " -f2)"
-  DELETIONS_COUNT="$(git diff --shortstat | tr "," "\n" | grep "deletion" | cut -d" " -f2)"
+  CHANGED_COUNT="$(git diff --shortstat | tr "," "\n" | grep "chang" | cut -d" " -f2)"
+  INSERTIONS_COUNT="$(git diff --shortstat | tr "," "\n" | grep "ins" | cut -d" " -f2)"
+  DELETIONS_COUNT="$(git diff --shortstat | tr "," "\n" | grep "del" | cut -d" " -f2)"
 fi
 
 if [ "$CHANGED_COUNT" > 0 ]; then
