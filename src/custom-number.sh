abfbc335e366f1f25074f8211c8aea46b8b980e0
Alejandro Amaral <aamaral@logico.com.ar>
2023-12-15 19:16:14 -0300

    feat: added extra number formats and pane indicator

--- src/custom-number.sh
+++ src/custom-number.sh
@@ -1,39 +1,100 @@
 #!/usr/bin/env bash
 #
-ID=$1
+ID=$(($1))
+FORMAT=$2
 
-case $ID in
-  0)
-    echo "🯰"
-    ;;
-  1)
-    echo "🯱"
-    ;;
-  2)
-    echo "🯲"
-    ;;
-  3)
-    echo "🯳"
-    ;;
-  4)
-    echo "🯴"
-    ;;
-  5)
-    echo "🯵"
-    ;;
-  6)
-    echo "🯶"
-    ;;
-  7)
-    echo "🯷"
-    ;;
-  8)
-    echo "🯸"
-    ;;
-  9)
-    echo "🯹"
-    ;;
-  *)
-    echo $ID
-    ;;
-esac
+if [ "$FORMAT" = "-d" ]; then
+  case $ID in
+    0) echo "🯰";;
+    1) echo "🯱";;
+    2) echo "🯲";;
+    3) echo "🯳";;
+    4) echo "🯴";;
+    5) echo "🯵";;
+    6) echo "🯶";;
+    7) echo "🯷";;
+    8) echo "🯸";;
+    9) echo "🯹";;
+    *) echo $ID;;
+  esac
+fi
+
+if [ "$FORMAT" = "-s" ]; then
+  case $ID in
+    0) echo "󰎡";;
+    1) echo "󰎤";;
+    2) echo "󰎧";;
+    3) echo "󰎪";;
+    4) echo "󰎭";;
+    5) echo "󰎱";;
+    6) echo "󰎳";;
+    7) echo "󰎶";;
+    8) echo "󰎹";;
+    9) echo "󰎼";;
+    *) echo $ID;;
+  esac
+fi
+
+if [ "$FORMAT" = "-o" ]; then
+  case $ID in
+    0) echo "󰎣";;
+    1) echo "󰎦";;
+    2) echo "󰎩";;
+    3) echo "󰎬";;
+    4) echo "󰎮";;
+    5) echo "󰎰";;
+    6) echo "󰎵";;
+    7) echo "󰎸";;
+    8) echo "󰎻";;
+    9) echo "󰎾";;
+    *) echo $ID;;
+  esac
+fi
+
+if [ "$FORMAT" = "-O" ]; then
+  case $ID in
+    0) echo "󰎢";;
+    1) echo "󰎥";;
+    2) echo "󰎨";;
+    3) echo "󰎫";;
+    4) echo "󰎲";;
+    5) echo "󰎯";;
+    6) echo "󰎴";;
+    7) echo "󰎷";;
+    8) echo "󰎺";;
+    9) echo "󰎽";;
+    *) echo $ID;;
+  esac
+fi
+
+if [ "$FORMAT" = "-r" ]; then
+  case $ID in
+    0) echo " ";;
+    1) echo "󱂈";;
+    2) echo "󱂉";;
+    3) echo "󱂊";;
+    4) echo "󱂋";;
+    5) echo "󱂌";;
+    6) echo "󱂍";;
+    7) echo "󱂎";;
+    8) echo "󱂏";;
+    9) echo "󱂐";;
+    *) echo $ID;;
+  esac
+fi
+
+if [ "$FORMAT" = "-S" ]; then
+  case $ID in
+    0) echo "⁰";;
+    1) echo "¹";;
+    2) echo "²";;
+    3) echo "³";;
+    4) echo "⁴";;
+    5) echo "⁵";;
+    6) echo "⁶";;
+    7) echo "⁷";;
+    8) echo "⁸";;
+    9) echo "⁹";;
+    *) echo $ID;;
+  esac
+fi
--- tokyo-night.tmux
+++ tokyo-night.tmux
@@ -16,16 +16,18 @@ SCRIPTS_PATH="$CURRENT_DIR/src"
 
 cmus_status="#($SCRIPTS_PATH/cmus-tmux-statusbar.sh)"
 git_status="#($SCRIPTS_PATH/git-status.sh #{pane_current_path})"
-custom_number="#($SCRIPTS_PATH/custom-number.sh #{window_index})"
+custom_number="#($SCRIPTS_PATH/custom-number.sh #{window_index} -d)"
+custom_pane="#($SCRIPTS_PATH/custom-number.sh #P -O)"
 
 #+--- Bars LEFT ---+
 # Session name
 tmux set -g status-left "#[fg=#15161e,bg=#2b97fa,bold] #S #[fg=#2b97fa,bg=default,nobold,noitalics,nounderscore]"
+
 #+--- Windows ---+
 # Focus
-tmux set -g window-status-current-format "#[fg=#44dfaf,bg=#1F2335]   #[fg=#a9b1d6,bg=#1F2335,bold]$custom_number #W #{?window_last_flag,,} "
+tmux set -g window-status-current-format "#[fg=#44dfaf,bg=#1F2335]   #[fg=#a9b1d6,bg=#1F2335]$custom_number #[bold,nodim]#W#[nobold,dim]#{?pane_index, $custom_pane,} #{?window_last_flag,,} "
 # Unfocused
-tmux set -g window-status-format "#[fg=#c0caf5,bg=default,none,dim]   $custom_number #W #[fg=yellow,blink]#{?window_last_flag,󰁯 ,} "
+tmux set -g window-status-format "#[fg=#c0caf5,bg=default,none,dim]   $custom_number #W#[nobold,dim]#{?pane_index, $custom_pane,}#[fg=yellow,blink] #{?window_last_flag,󰁯 ,} "
 
 #+--- Bars RIGHT ---+
 tmux set -g status-right "$cmus_status#[fg=#a9b1d6,bg=#24283B]  %Y-%m-%d #[]❬ %H:%M $git_status"
