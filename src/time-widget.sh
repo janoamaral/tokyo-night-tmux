81cf65b281b0921089c2da8edf6d07df3aef3c64
AustinCGause <austincgause@gmail.com>
2024-03-24 13:38:12 -0400

    fixed variable usage

--- src/time-widget.sh
+++ src/time-widget.sh
@@ -4,29 +4,29 @@
 date_format="$1"
 time_format="$2"
 
-date_string = ""
-time_string = ""
+date_string=""
+time_string=""
 
 if [[ "$date_format" == "YMD" ]]; then
   # Year Month Day Date Format
-  date_string = "%Y-%m-%d"
+  date_string="%Y-%m-%d"
 elif [[ "$date_format" == "MDY" ]]; then
   # Month Day Year Date Format
-  date_string = "%m-%d-%Y"
+  date_string="%m-%d-%Y"
 elif [[ "$date_format" == "DMY" ]]; then
   # Day Month Year Date Format
-  date_string = "%d-%m-%Y"
+  date_string="%d-%m-%Y"
 else
   # Default to YMD Date Format
-  date_string = "%Y-%m-%d"
+  date_string="%Y-%m-%d"
 fi
 
 if [[ "$time_format" == "12H" ]]; then
     # 12-hour format with AM/PM
-    time_string = "%I:%M %p"
+    time_string="%I:%M %p"
 else
     # Default to 24-hour format
-    time_string = "%H:%M"
+    time_string="%H:%M"
 fi
 
 echo "#[fg=#a9b1d6,bg=#24283B] $date_string #[]❬ $time_string"
