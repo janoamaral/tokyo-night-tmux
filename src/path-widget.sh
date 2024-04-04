da23a7f82be72f41d84fe862cc37c2bfcdeeb0c0
Alejandro Amaral <aamaral@logico.com.ar>
2024-04-04 14:38:17 -0300

    fix: better styling

--- src/path-widget.sh
+++ src/path-widget.sh
@@ -3,9 +3,10 @@
 # get value from tmux config
 SHOW_PATH=$(tmux show-option -gv @tokyo-night-tmux_show_path 2>/dev/null)
 PATH_FORMAT=$(tmux show-option -gv @tokyo-night-tmux_path_format 2>/dev/null) # full | relative
+RESET="#[fg=brightwhite,bg=#15161e,nobold,noitalics,nounderscore,nodim]"
 
 # check if not enabled
-if [ "${SHOW_PATH}" == "0" ]; then
+if [ "${SHOW_PATH}" != "1" ]; then
     exit 0
 fi
 
@@ -18,4 +19,4 @@ if [[ "${PATH_FORMAT}" == "relative" ]]; then
   current_path="$(echo ${current_path} |  sed 's#'"$HOME"'#~#g')"
 fi
 
-echo "${current_path} ‹ "
+echo "#[fg=blue,bg=default]░  ${RESET}#[bg=default]${current_path} "
