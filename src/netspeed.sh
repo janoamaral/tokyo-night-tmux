69a53bb4bacf984c7bc2159c602a8a213ffb758f
Daniel Porter <dpreid@gmail.com>
2024-04-17 02:55:22 +0100

    netspeed feature rework
    This implements the features suggested in #66:
    - Detect when the interface is down
    - Add config flag to show local IPv4 address
    - Updated icons for interface types and state
    
    The following additional changes were implemented:
    - Reduce to single bc calls for netspeed calculations
    - Add interface auto-detection via route
    - Add variable refresh time

--- src/netspeed.sh
+++ src/netspeed.sh
@@ -4,70 +4,71 @@
 # email : freakybytes@duck.com
 #<------------------------------------------------------------------------------------------>
 
+# Check if enabled
+ENABLED=$(tmux show-option -gv @tokyo-night-tmux_show_netspeed 2>/dev/null)
+[[ ${ENABLED} -ne 1 ]] && exit 0
+
 # Imports
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
-. "${ROOT_DIR}/lib/coreutils-compat.sh"
-
-# Check the global value
-SHOW_NETSPEED=$(tmux show-option -gv @tokyo-night-tmux_show_netspeed)
-if [ "$SHOW_NETSPEED" != "1" ]; then
-  exit 0
-fi
-
-CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source $CURRENT_DIR/themes.sh
+source "$ROOT_DIR/src/themes.sh"
+source "$ROOT_DIR/lib/netspeed.sh"
 
 # Get network interface
 INTERFACE=$(tmux show-option -gv @tokyo-night-tmux_netspeed_iface 2>/dev/null)
+# Show IP address
+SHOW_IP=$(tmux show-option -gv @tokyo-night-tmux_netspeed_showip 2>/dev/null)
+# Time between refresh
+TIME_DIFF=$(tmux show-option -gv @tokyo-night-tmux_netspeed_refresh 2>/dev/null)
+TIME_DIFF=${TIME_DIFF:-1}
 
-# Get network transmit data from /proc/net/dev
-get_bytes() {
-  local interface="$1"
-  if [[ "$(uname)" == "Linux" ]]; then
-    awk -v interface="$interface" '$1 == interface ":" {print $2, $10}' /proc/net/dev
-  elif [[ "$(uname)" == "Darwin" ]]; then
-    netstat -ib | awk -v interface="$interface" '/^'${interface}'/ {print $7, $10}'
-  else
-    # Unsupported operating system
-    exit 1
-  fi
-}
+# Icons
+declare -A NET_ICONS
+NET_ICONS[wifi_up]="#[fg=${THEME[foreground]}]\U000f05a9"  # nf-md-wifi
+NET_ICONS[wifi_down]="#[fg=${THEME[red]}]\U000f05aa"       # nf-md-wifi_off
+NET_ICONS[wired_up]="#[fg=${THEME[foreground]}]\U000f0318" # nf-md-lan_connect
+NET_ICONS[wired_down]="#[fg=${THEME[red]}]\U000f0319"      # nf-md-lan_disconnect
+NET_ICONS[traffic_tx]="#[fg=${THEME[bblue]}]\U000f0cd8"    # nf-md-upload_network_outline
+NET_ICONS[traffic_rx]="#[fg=${THEME[bgreen]}]\U000f0c66"   # nf-md-download_network_outline
+NET_ICONS[ip]="#[fg=${THEME[foreground]}]\U000f0a5f"       # nf-md-ip
 
-# Convert into readable format
-readable_format() {
-  local bytes=$1
-
-  # Convert bytes to KBps, 'bc' is dependency, 'pacman -S bc'
-  local kbps=$(echo "scale=1; $bytes / 1024" | bc)
-  if (($(echo "$kbps < 1" | bc -l))); then
-    echo "0.0B"
-  elif (($(echo "$kbps >= 1024" | bc -l))); then
-    # Convert KBps to MBps
-    local mbps=$(echo "scale=1; $kbps / 1024" | bc)
-    echo "${mbps}MB/s"
-  else
-    echo "${kbps}KB/s"
-  fi
-}
+# Determine interface if not set
+if [[ -z $INTERFACE ]]; then
+  INTERFACE=$(find_interface)
+  [[ -z $INTERFACE ]] && exit 1
+  # Update tmux option for this session
+  tmux set-option -g @tokyo-night-tmux_netspeed_iface "$INTERFACE"
+fi
 
 # Echo network speed
-read RX1 TX1 < <(get_bytes "$INTERFACE")
-sleep 1
-read RX2 TX2 < <(get_bytes "$INTERFACE")
+read -r RX1 TX1 < <(get_bytes "$INTERFACE")
+sleep "$TIME_DIFF"
+read -r RX2 TX2 < <(get_bytes "$INTERFACE")
 
 RX_DIFF=$((RX2 - RX1))
 TX_DIFF=$((TX2 - TX1))
 
-TIME_DIFF=1
+RX_SPEED="#[fg=${THEME[foreground]}]$(readable_format "$RX_DIFF" "$TIME_DIFF")"
+TX_SPEED="#[fg=${THEME[foreground]}]$(readable_format "$TX_DIFF" "$TIME_DIFF")"
 
-RX_SPEED=$(readable_format "$((RX_DIFF / TIME_DIFF))")
-TX_SPEED=$(readable_format "$((TX_DIFF / TIME_DIFF))")
-
-NETWORK_ICON="󰈀"
-
-# TODO: Use a more sophisticated method to detect iface type (wifi, ethernet, etc)
-if [ ${INTERFACE:0:1} == "w" ]; then
-  NETWORK_ICON=""
+# Interface icon
+if [[ ${INTERFACE} == "en0" ]] || [[ ${INTERFACE:0:1} == "w" ]]; then
+  IFACE_TYPE="wifi"
+else
+  IFACE_TYPE="wired"
 fi
 
-echo "${RESET}░ #[fg=${THEME[bgreen]}]󰛴${RESET} $RX_SPEED #[fg=${THEME[bblue]}]󰛶${RESET} $TX_SPEED ${NETWORK_ICON} #[dim]$INTERFACE "
+# Detect interface IPv4 and state
+if IPV4_ADDR=$(interface_ipv4 "$INTERFACE"); then
+  IFACE_STATUS="up"
+else
+  IFACE_STATUS="down"
+fi
+
+NETWORK_ICON=${NET_ICONS[${IFACE_TYPE}_${IFACE_STATUS}]}
+
+OUTPUT="${RESET}░ ${NET_ICONS[traffic_rx]} $RX_SPEED ${NET_ICONS[traffic_tx]} $TX_SPEED $NETWORK_ICON #[dim]$INTERFACE "
+if [[ ${SHOW_IP} -ne 0 ]] && [[ -n $IPV4_ADDR ]]; then
+  OUTPUT+="${NET_ICONS[ip]} #[dim]$IPV4_ADDR "
+fi
+
+echo -e "$OUTPUT"
