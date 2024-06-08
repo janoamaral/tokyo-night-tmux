aefa3f04f39a6dd0a92bd2719f318b4b774720b9
Alejandro Amaral <aamaral@logico.com.ar>
2024-06-08 20:33:31 -0300

    feat: added hide style to custom numbers (#89)

--- src/custom-number.sh
+++ src/custom-number.sh
@@ -4,6 +4,7 @@
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
 . "${ROOT_DIR}/lib/coreutils-compat.sh"
 
+format_hide=""
 format_none="0123456789"
 format_digital="🯰🯱🯲🯳🯴🯵🯶🯷🯸🯹"
 format_fsquare="󰎡󰎤󰎧󰎪󰎭󰎱󰎳󰎶󰎹󰎼"
@@ -18,6 +19,11 @@ FORMAT=${2:-none}
 
 # Preserve leading whitespace for bash
 format="$(eval echo \"\$format_${FORMAT}\")"
+
+if [ "$FORMAT" = "hide" ]; then
+  exit 0
+fi
+
 if [ -z "$format" ]; then
   echo "Invalid format: $FORMAT"
   exit 1
@@ -25,11 +31,10 @@ fi
 
 # If format is roman numerals (-r), only handle IDs of 1 digit
 if [ "$FORMAT" = "roman" ] && [ ${#ID} -gt 1 ]; then
-  echo -n $ID
-  continue
+  echo -n "$ID "
 else
   for ((i = 0; i < ${#ID}; i++)); do
     DIGIT=${ID:i:1}
-    echo -n "${format:DIGIT:1}"
+    echo -n "${format:DIGIT:1} "
   done
 fi
