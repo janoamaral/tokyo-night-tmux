55ad0db4a493ae6c1299342b71de358dc0bb93f1
thassiov <tvmcarvalho@gmail.com>
2024-08-06 16:07:13 -0300

    Make hostname widget disabled by default

--- src/hostname-widget.sh
+++ src/hostname-widget.sh
@@ -4,16 +4,13 @@
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
 . "${ROOT_DIR}/lib/coreutils-compat.sh"
 
-# Grab global variable for showing hostname widget, only hide if explicitly disabled
-SHOW_HOSTNAME=$(tmux show-option -gv @tokyo-night-tmux_show_hostname 2>/dev/null)
-if [[ $SHOW_HOSTNAME == "0" ]]; then
-  exit 0
-fi
+# Check if enabled
+ENABLED=$(tmux show-option -gv @tokyo-night-tmux_show_hostname 2>/dev/null)
+[[ ${ENABLED} -ne 1 ]] && exit 0
 
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $CURRENT_DIR/themes.sh
 
 hostname=$(hostnamectl hostname)
-chassis_icon=$(hostnamectl | grep Chassis | xargs |  cut -d " " -f3)
 
 echo "#[fg=red,bold,bg=default]░ @ #[fg=brightwhite,bg=${THEME[background]}]${hostname}"
