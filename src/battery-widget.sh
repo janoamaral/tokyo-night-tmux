9f8a092a3e2cf6f016303b6de5652467fe31da29
Shawn-Summer <362847502@qq.com>
2025-10-12 09:23:59 +0800

    bug fixed

--- src/battery-widget.sh
+++ src/battery-widget.sh
@@ -121,4 +121,4 @@ else
 fi
 
 # Print the battery status with some extra spaces for padding
-echo "${color}░ ${ICON}${RESET} #[bg=default] ${BATTERY_PERCENTAGE}% "
+echo "${color}░ ${ICON}${RESET}#[bg=default] ${BATTERY_PERCENTAGE}% "
